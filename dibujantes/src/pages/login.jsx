a68ee504b86df35639529e5a0a5bab9e40d10743
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-27 13:24:30 +0000

    Probando

---
+++ dibujantes/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const login = () => {
+
+    return (
+
+        <div className= "loginPage">
+
+            <h1 className= "loginPage-title">Log in to Twitter</h1>
+        
+        </div >
+        
+        );
+        
+}
